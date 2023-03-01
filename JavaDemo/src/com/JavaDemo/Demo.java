ced6379dd46900521b249f263100525d52ad31bd
IT Solution <IT Solution@DESKTOP-2MKJR6R>
2023-02-28 22:06:48 -0800

    FirstCommit

---
+++ JavaDemo/src/com/JavaDemo/Demo.java
@@ -0,0 +1,12 @@
+package com.JavaDemo;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ JavaDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author IT Solution
+ *
+ */
+module JavaDemo {
+}
