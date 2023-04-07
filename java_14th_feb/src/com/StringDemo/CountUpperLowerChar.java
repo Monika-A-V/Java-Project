04b4a577e262b591e9c844fbcf989a8c07ac5cda
IT Solution <IT Solution@DESKTOP-2MKJR6R>
2023-04-07 01:11:11 -0700

    commit3

---
+++ java_14th_feb/src/com/StringDemo/CountUpperLowerChar.java
@@ -0,0 +1,30 @@
+package com.StringDemo;
+
+public class CountUpperLowerChar {
+
+	public static void main(String[] args) {
+
+		String s="MoNiKa";
+		int upper=0;
+		int lower=0;
+		char ch[]=s.toCharArray();
+		for(int i=0;i<ch.length;i++)
+		{
+			if(ch[i]>='A' && ch[i]<='Z')
+			{
+				upper++;
+			}
+			else if(ch[i]>='a' && ch[i]<='z')
+			{
+				lower++;
+			}
+			else
+			{
+				continue;
+			}
+				
+		}
+		System.out.println("upper count is = "+upper+" and lower count is = "+lower);
+	}
+
+}
---
+++ java_14th_feb/src/com/StringDemo/IntegralValueStringArray.java
@@ -0,0 +1,84 @@
+package com.StringDemo;
+
+import java.util.Scanner;
+public class IntegralValueStringArray
+{
+
+	public static void main(String[] args)
+	{
+		String[] s = {"A+2", "D+3", "B+7", "E+1"};
+
+		for (int i=0; i<s.length; i++) 
+		{
+		    char c = s[i].charAt(0);
+		    int integralValue = s[i].charAt(2) - '0';
+		    System.out.println("Value with " + c + " is " + integralValue);
+		}
+
+		    }
+}
+               /*( Scanner sc = new Scanner(System.in);
+		        String[] s = {"A+2", "D+3", "B+7", "E+1"};
+
+		        System.out.println("Enter a character:");
+		        char c1 = sc.next().charAt(0);
+
+		        for (String str : s) {
+		            char[] chars = str.toCharArray();
+		            for (int i = 0; i < chars.length; i++) {
+		                if (chars[i] == c1) {
+		                    int integralValue = Character.getNumericValue(chars[i+1]);
+		                    System.out.println("integral value of char " + chars[i] + " is = " + integralValue);
+		                    break;
+		                }
+		            }
+		        }
+		    */
+
+		/*Scanner sc = new Scanner(System.in);
+		String s[] = {"A+2","D+3","B+7","E+1"};
+		char[] result = new char[s.length * 3];
+
+		System.out.println("Enter a character:");
+		char c1 = sc.next().charAt(0);
+		int index = 0;
+
+		for (int i = 0; i < s.length; i++)
+		{
+		    char[] c = s[i].toCharArray();
+		    for (int j = 0; j < c.length; j++)
+		    {
+		        result[index] = c[j];
+		        index++;
+
+		        if (c[j] == c1)
+		        {
+		            int i1 = (int) c[j];
+		            System.out.println("integral value of char " +c[j]+" is = " +i1);
+		        }
+		    }
+		}*/
+
+		/*Scanner sc=new Scanner(System.in);
+		String s[] = {"A+2","D+3","B+7","E+1"};
+		char[] result = new char[s.length * 3]; 
+
+		System.out.println("enter char");
+		char c1=sc.next().charAt(0);
+		int index = 0;
+		for (String str : s) 
+		{
+		    char[] chars = str.toCharArray();
+		    for (char c : chars) 
+		    {
+		        result[index++] = c;
+		        if(c==c1)
+		        {
+		        	int i1 = (int)c;
+                    System.out.println(i1); 
+	}
+		    }}*/
+		
+
+
+
